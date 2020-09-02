4db1435f8fe78833d366c70ec288f1cf06845cbd
shoooooman <shoma216@gmail.com>
2020-09-02 09:43:38 +0900

    fix import path in main.go

--- cmd/complexity/main.go
+++ cmd/complexity/main.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-	"github.com/shoooooman/complexity"
+	"github.com/shoooooman/go-complexity-analysis"
 	"golang.org/x/tools/go/analysis/unitchecker"
 )
 
 func main() { unitchecker.Main(complexity.Analyzer) }
-

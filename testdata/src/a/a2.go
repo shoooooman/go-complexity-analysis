93ade84e226c5895072ce3e163c21867cde50b2b
shoooooman <shoma216@gmail.com>
2020-09-03 14:45:38 +0900

    add a file for reviewdog test

---
+++ testdata/src/a/a2.go
@@ -0,0 +1,33 @@
+package a
+
+func f4() {
+	if false {
+
+	} else if false {
+
+	} else if false {
+
+	} else if false {
+
+	} else if false {
+
+	} else if false {
+
+	} else if false {
+
+	} else if false {
+
+	} else if false {
+
+	} else if false {
+
+	} else if false {
+
+	} else if false {
+
+	} else if false {
+
+	} else {
+
+	}
+}

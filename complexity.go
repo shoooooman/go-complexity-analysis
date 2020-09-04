5b97211e06d41a691829453c05e7aa36e20ba138
shoooooman <shoma216@gmail.com>
2020-09-04 11:27:33 +0900

    add dummy func

--- complexity.go
+++ complexity.go
@@ -413,3 +413,21 @@ func appendValidSymb(lvalid bool, rvalid bool, opt map[string]int, symb string) 
 		opt[symb]++
 	}
 }
+
+func dummy() {
+	n := 0
+	switch n {
+	case 0:
+	case 1:
+	case 2:
+	case 3:
+	case 4:
+	case 5:
+	case 6:
+	case 7:
+	case 8:
+	case 9:
+	case 10:
+	case 11:
+	}
+}
